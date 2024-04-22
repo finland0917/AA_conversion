b92830e766496f5767340e45e2ccde025e219e17
finland0917 <92002704+finland0917@users.noreply.github.com>
2024-04-23 02:21:58 +0900

    Update AA_conversion.py

--- AA_conversion.py
+++ AA_conversion.py
@@ -1,17 +1,21 @@
 from PIL import Image
 import sys
 
-# グレースケールに変換する際に使用する文字列（濃い→薄いの順）
 # 濃い色から薄い色へと階調を表現するために、複数の文字を使用
 ascii_chars = '@%#*+=-:. '
 
 def image_to_ascii(image_path, output_width):
+    
     # 画像を開く
     img = Image.open(image_path)
     
     # 画像のアスペクト比を維持してリサイズ
-    aspect_ratio = img.width / img.height
+    aspect_ratio = img.width / img.heightｃ
+
+    # 高さを計算
     output_height = int(output_width / aspect_ratio)
+
+    # 画像を指定したサイズにリサイズ
     img = img.resize((output_width, output_height))
     
     # 画像をグレースケールに変換
@@ -23,17 +27,15 @@ def image_to_ascii(image_path, output_width):
         for x in range(output_width):
             pixel_value = img.getpixel((x, y))
             # ピクセルの輝度値に基づいて適切な文字を選択
-            # 0から255の輝度値を、0から文字列の長さ-1の範囲に変換
-            # そして、対応する文字を選択してAA文字列に追加
             ascii_str += ascii_chars[int(pixel_value / 255 * (len(ascii_chars) - 1))]
         ascii_str += '\n'  # 行の終わりで改行を追加
     
     return ascii_str
 
 def main():
-    # コマンドライン引数から入力ファイルのパスと出力の横幅を取得
+    # コマンドライン引数から入力ファイルのパスと出力の横幅を入力
     if len(sys.argv) != 3:
-        print("Usage: python script.py input_image_path output_width")
+        print("例:python script.py input_image_path output_width")
         return
     input_image_path = sys.argv[1]
     output_width = int(sys.argv[2])
